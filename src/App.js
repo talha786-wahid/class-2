55c8ca376a3c8130f11f6fb80202567759e8f5cf
Muhammad Talha <talhawahid886@gmail.com>
2021-01-24 16:27:23 +0500

    ap.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import store from "./store";
-import Landing from "./container/ReduxPractice/Landing";
-import CheckoutProduct from "./container/ReduxPractice/CheckoutProduct";
-import PostForm from "./container/ReduxPractice/Redux/components/PostForm";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      {/* <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/checkoutProduct" component={CheckoutProduct} />
-          </Switch>
-        </div>
-      </Router> */}
-      <PostForm />
+      <h1>Talha</h1>
     </div>
   );
 }
